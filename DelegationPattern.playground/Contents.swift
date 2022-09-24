fa6e6fbaf3b60e5182f1333ea42aadbea283abc1
Uladzislau_Komar <vladkomar25@gmail.com>
2022-09-24 16:38:54 +0300

    Added axemple with the DelegationPattern

--- DelegationPattern.playground/Contents.swift
+++ DelegationPattern.playground/Contents.swift
@@ -1,3 +1,34 @@
-import Cocoa
+//MARK: Protocols
+protocol AccountingDelegate {
+    mutating func doAccounting()
+}
 
-var greeting = "Hello, playground"
+protocol DirectorProtocol {
+    var accountantDel: AccountingDelegate? { get set }
+    mutating func doAccounting()
+    mutating func doManagement()
+}
+
+//Structures
+
+struct Accountant: AccountingDelegate {
+    func doAccounting() {
+        print("Accountant does accounting")
+    }
+}
+
+struct Director {
+    var accountantDel: AccountingDelegate?
+    mutating func doAccounting() {
+        accountantDel?.doAccounting()
+    }
+    mutating func doManagement() {
+        print("Director does managament work")
+    }
+}
+
+var director = Director()
+director.accountantDel = Accountant()
+director.doManagement()
+director.doAccounting()
+
