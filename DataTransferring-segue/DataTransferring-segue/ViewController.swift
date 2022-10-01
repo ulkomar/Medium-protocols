f7aad8fb21c20b8f14f81abf66a47bd41a7784c8
Uladzislau_Komar <vladkomar25@gmail.com>
2022-10-01 18:16:30 +0300

    Added a new project for data transferring with segue

---
+++ DataTransferring-segue/DataTransferring-segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataTransferring-segue
+//
+//  Created by Uladzislau Komar on 1.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
