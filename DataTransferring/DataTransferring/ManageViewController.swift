7ca4e0a02d3c788960a8270c4adf15a5640f72e7
Uladzislau_Komar <vladkomar25@gmail.com>
2022-10-01 15:26:05 +0300

    created 2 ViewControllers

---
+++ DataTransferring/DataTransferring/ManageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ManageViewController.swift
+//  DataTransferring
+//
+//  Created by Uladzislau Komar on 1.10.22.
+//
+
+import UIKit
+
+class ManageViewController: UIViewController {
+    //MARK: IBOutlets
+    @IBOutlet var textField: UITextField!
+    
+    //MARK: Overriding functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: Auxiliary functions
+
+    //MARK: IBActions
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DataTransferring/DataTransferring/ViewController.swift
+++ DataTransferring/DataTransferring/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: IBOutlets
+    @IBOutlet var textLabel: UILabel!
+    
+    //MARK: Overriding functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: Auxiliary functions
 
-
+    //MARK: IBActions
+    
 }
 
