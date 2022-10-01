f5a807f40ea6bfc0bf9046a1ab26bc4a9c941cc0
Uladzislau_Komar <vladkomar25@gmail.com>
2022-10-01 14:05:31 +0300

    Deleted segues between view controllers

--- NavigationApp/NavigationApp/ViewController.swift
+++ NavigationApp/NavigationApp/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toGreenScene(_ sender: UIButton) {
+        let greenScene = storyBoard.instantiateViewController(withIdentifier: "greenViewController")
+        self.navigationController?.pushViewController(greenScene, animated: true)
+    }
+    
+    @IBAction func toYellowScene(_ sender: UIButton) {
+        let yellowScene = storyBoard.instantiateViewController(withIdentifier: "yellowViewController")
+        self.navigationController?.pushViewController(yellowScene, animated: true)
+    }
+    
+    @IBAction func toRootScene(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
 }
 
