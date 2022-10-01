9564d8e31756b35d2f99955f02ea452084c982e1
Uladzislau_Komar <vladkomar25@gmail.com>
2022-10-01 20:39:54 +0300

    Added template for data delegation transferring

---
+++ DataTransfering-Delegation/DataTransfering-Delegation/ManageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ManageViewController.swift
+//  DataTransfering-Delegation
+//
+//  Created by Uladzislau Komar on 1.10.22.
+//
+
+import UIKit
+
+class ManageViewController: UIViewController {
+
+    @IBOutlet var textField: UITextField!
+    var updatingTextField: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
---
+++ DataTransfering-Delegation/DataTransfering-Delegation/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DataTransfering-Delegation
+//
+//  Created by Uladzislau Komar on 1.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var textLabel: UILabel!
+    var updatedLabel: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
+
