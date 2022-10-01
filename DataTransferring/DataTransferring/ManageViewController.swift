d218e5a3c5ef5e2399553da8cade2577628c98e4
Uladzislau_Komar <vladkomar25@gmail.com>
2022-10-01 16:58:40 +0300

    Created 2 views with the property data transferring

--- DataTransferring/DataTransferring/ManageViewController.swift
+++ DataTransferring/DataTransferring/ManageViewController.swift
@@ -8,28 +8,35 @@
 import UIKit
 
 class ManageViewController: UIViewController {
+    //MARK: Properties
+    var updatingLabelText: String = ""
+    
     //MARK: IBOutlets
     @IBOutlet var textField: UITextField!
     
     //MARK: Overriding functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.updateDataWithProperties(text: self.updatingLabelText)
     }
     
     //MARK: Auxiliary functions
+    private func updateDataWithProperties(text: String) {
+        self.textField.text = text
+    }
 
     //MARK: IBActions
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveWithProperties(_ sender: UIButton) {
+        self.navigationController?.viewControllers.forEach {controller in
+            guard let firstController = controller as? ViewController else {
+                return
+            }
+            firstController.updatedLabel = self.textField.text ?? ""
+            self.navigationController?.popViewController(animated: true)
+        }
     }
-    */
-
 }
--- DataTransferring/DataTransferring/ViewController.swift
+++ DataTransferring/DataTransferring/ViewController.swift
@@ -8,18 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: Properties
+    var updatedLabel: String = "Testing"
+    
     //MARK: IBOutlets
     @IBOutlet var textLabel: UILabel!
     
     //MARK: Overriding functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.updatePropertyLabel(text: updatedLabel)
     }
     
     //MARK: Auxiliary functions
+    private func updatePropertyLabel(text: String) {
+        self.textLabel.text = self.updatedLabel
+    }
 
     //MARK: IBActions
-    
+    @IBAction func editDataWithProperties(_ sender: UIButton) {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let manageScreen = storyBoard.instantiateViewController(withIdentifier: "manageViewController") as! ManageViewController
+        manageScreen.updatingLabelText = textLabel.text ?? ""
+        
+        self.navigationController?.pushViewController(manageScreen, animated: true)
+    }
 }
 
