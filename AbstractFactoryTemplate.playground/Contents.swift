a669163d02c2e67a53bb6ab12bad3cb6577897f1
Uladzislau_Komar <vladkomar25@gmail.com>
2022-10-02 17:12:38 +0300

    Practice factory pattern

---
+++ AbstractFactoryTemplate.playground/Contents.swift
@@ -0,0 +1,65 @@
+protocol CarsFactoryProtocol {
+    func createSendan() -> Sedan
+    func createCoupe() -> Coupe
+}
+
+protocol Sedan {
+    var type: String { get set }
+    func sayWho()
+}
+
+protocol Coupe {
+    var type: String { get set }
+    func sayWho()
+}
+
+class ToyotaCoupe: Coupe {
+    var type = "ToyotaCoupe"
+    func sayWho() {
+        print("I'm \(type)")
+    }
+}
+
+class ToyotaSedan: Sedan {
+    var type = "ToyotaSedan"
+    func sayWho() {
+        print("I'm \(type)")
+    }
+}
+
+class FordCoupe: Coupe {
+    var type = "FordCoupe"
+    func sayWho() {
+        print("I'm \(type)")
+    }
+}
+
+class FordSedan: Sedan {
+    var type = "FordSedan"
+    func sayWho() {
+        print("I'm \(type)")
+    }
+}
+
+class ToyotaFactory: CarsFactoryProtocol {
+    func createSendan() -> Sedan {
+        return ToyotaSedan()
+    }
+    func createCoupe() -> Coupe {
+        return ToyotaCoupe()
+    }
+}
+
+class FordFactory: CarsFactoryProtocol {
+    func createSendan() -> Sedan {
+        return FordSedan()
+    }
+    func createCoupe() -> Coupe {
+        return FordCoupe()
+    }
+}
+
+var carFactory = ToyotaFactory()
+var carSedan = carFactory.createSendan()
+carSedan.sayWho()
+ 
