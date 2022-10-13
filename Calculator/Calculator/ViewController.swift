d7990b73d3bea19fc64a3c19955e9d988317f803
Uladzislau_Komar <vladkomar25@gmail.com>
2022-10-13 16:50:06 +0300

    Prepared UI

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    enum Operators: String {
+        case plus = "+"
+        case minus = "-"
+        case multiply = "*"
+        case equal = "="
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonCLick(_ sender: UIButton) {
+        guard let buttonText = sender.title(for: .normal) else {
+            return
+        }
+        
+        var labelText = self.label.text ?? ""
+        labelText += buttonText
+        self.label.text = labelText
+    }
+    
+    
 }
 
