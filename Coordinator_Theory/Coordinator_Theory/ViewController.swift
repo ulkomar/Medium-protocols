0e5b186b2dbc80af7ea1a2ef89465f935a56ce33
Uladzislau_Komar <vladkomar25@gmail.com>
2022-10-26 20:21:06 +0300

    Prepared template for medium article

---
+++ Coordinator_Theory/Coordinator_Theory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coordinator_Theory
+//
+//  Created by Uladzislau Komar on 25.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
